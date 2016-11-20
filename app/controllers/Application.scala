3a31eb12dd928288401c7daa7283e52d5573f1f0
bvenners <bill@artima.com>
2016-11-20 17:56:57 -0500

    Change latestVersion in Application controller from 3.0.0 to 3.0.1.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -20,13 +20,13 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  val latestScaladoc = "http://doc.scalatest.org/3.0.0"
-  val latestVersion = "3.0.0"
+  val latestScaladoc = "http://doc.scalatest.org/3.0.1"
+  val latestVersion = "3.0.1"
   val latestSuperSafeVersion = "1.1.0"
-  val milestoneVersion = "3.0.0-RC4"
-  val milestoneJar = "https://oss.sonatype.org/content/groups/public/org/scalactic/scalactic_2.11/3.0.0-RC4/scalactic_2.10-3.0.0-RC4.jar"
-  val latestJar = "https://oss.sonatype.org/content/groups/public/org/scalactic/scalactic_2.11/3.0.0/scalactic_2.11-3.0.0.jar"
-  val milestoneScaladoc = "http://www.artima.com/docs-scalatest-3.0.0-RC4"
+  val milestoneVersion = "3.0.1-RC4"
+  val milestoneJar = "https://oss.sonatype.org/content/groups/public/org/scalactic/scalactic_2.11/3.0.1-RC4/scalactic_2.10-3.0.1-RC4.jar"
+  val latestJar = "https://oss.sonatype.org/content/groups/public/org/scalactic/scalactic_2.11/3.0.1/scalactic_2.11-3.0.1.jar"
+  val milestoneScaladoc = "http://www.artima.com/docs-scalatest-3.0.1-RC4"
   val scaladocsLocation = "http://doc.scalatest.org"
   val releasesLocation = "http://www.artima.com/downloadScalaTest"
   val baseScalaVersion = "2.11.0"
