4cdb3d406b51141f144fa06a33f45243995e04d7
Chua Chee Seng <cheeseng@amaseng.com>
2020-04-30 20:47:13 +0800

    Bumped up base scala version to 2.13.2.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -69,7 +69,7 @@ object Application {
   val milestoneScaladoc = "http://www.artima.com/docs-scalatest-3.1.0-RC3"
   val scaladocsLocation = "http://doc.scalatest.org"
   val releasesLocation = "http://www.artima.com/downloadScalaTest"
-  val baseScalaVersion = "2.13.1"			
+  val baseScalaVersion = "2.13.2"			
   val majorMinorScalaVersion = "2.13"
 
 }
