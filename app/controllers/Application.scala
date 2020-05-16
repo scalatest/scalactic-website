3144940882a3514439475df59e604f4b84e21292
bvenners <bill@artima.com>
2020-05-16 15:48:06 -0700

    Update to 3.1.2

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -61,11 +61,11 @@ object Application {
 
   val latestScaladoc = "http://doc.scalatest.org/3.1.1"
   val latestScalacticScaladoc = "http://doc.scalactic.org/3.1.1"
-  val latestVersion = "3.1.1"
+  val latestVersion = "3.1.2"
   val latestSuperSafeVersion = "1.1.10"
   val milestoneVersion = "3.1.0-RC3"
   val milestoneJar = "https://oss.sonatype.org/content/groups/public/org/scalactic/scalactic_2.13/3.1.0-RC3/scalactic_2.10-3.1.0-RC3.jar"
-  val latestJar = "https://oss.sonatype.org/content/groups/public/org/scalactic/scalactic_2.13/3.1.1/scalactic_2.13-3.1.1.jar"
+  val latestJar = "https://oss.sonatype.org/content/groups/public/org/scalactic/scalactic_2.13/3.1.1/scalactic_2.13-3.1.2.jar"
   val milestoneScaladoc = "http://www.artima.com/docs-scalatest-3.1.0-RC3"
   val scaladocsLocation = "http://doc.scalatest.org"
   val releasesLocation = "http://www.artima.com/downloadScalaTest"
