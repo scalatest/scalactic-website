aab592abc8ec9c969e8f3c21a145f9fe5c09cafb
Chua Chee Seng <cheeseng@amaseng.com>
2020-09-23 11:53:04 +0800

    Updated Application.latestVersion to 3.2.2, and Application.baseScalaVersion to 2.13.3.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -61,7 +61,7 @@ object Application {
 
   val latestScaladoc = "http://doc.scalatest.org/3.2.0"
   val latestScalacticScaladoc = "http://doc.scalactic.org/3.2.0"
-  val latestVersion = "3.2.0"
+  val latestVersion = "3.2.2"
   val latestSuperSafeVersion = "1.1.10"
   val milestoneVersion = "3.1.0-RC3"
   val milestoneJar = "https://oss.sonatype.org/content/groups/public/org/scalactic/scalactic_2.13/3.1.0-RC3/scalactic_2.10-3.1.0-RC3.jar"
@@ -69,6 +69,6 @@ object Application {
   val milestoneScaladoc = "http://www.artima.com/docs-scalatest-3.1.0-RC3"
   val scaladocsLocation = "http://doc.scalatest.org"
   val releasesLocation = "http://www.artima.com/downloadScalaTest"
-  val baseScalaVersion = "2.13.2"			
+  val baseScalaVersion = "2.13.3"			
   val majorMinorScalaVersion = "2.13"
 }
